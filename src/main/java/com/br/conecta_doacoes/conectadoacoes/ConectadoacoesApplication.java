f8bc391c398b2126e761174d2c3177011d1bf1a4
Guilherme Sales <guilhermessales11@gmail.com>
2025-07-24 14:42:54 -0300

    feat: Iniciado projeto

---
+++ src/main/java/com/br/conecta_doacoes/conectadoacoes/ConectadoacoesApplication.java
@@ -0,0 +1,13 @@
+package com.br.conecta_doacoes.conectadoacoes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConectadoacoesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConectadoacoesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/conecta_doacoes/conectadoacoes/ConectadoacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.conecta_doacoes.conectadoacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConectadoacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
